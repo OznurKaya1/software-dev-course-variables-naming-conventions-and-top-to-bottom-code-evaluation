28232de0b894b129f284356edfdb7c3576e63842
Oznur <oznur@Oznurs-MacBook-Pro.local>
2025-07-11 15:23:32 -0500

    Your commit message

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -30,3 +30,31 @@ let c = 20;
 let d = a + " bought " + b + " items for $" + c + ".";
 
 console.log(d);
+
+let customerName = "Alice";
+let numberOfItems = 5;
+let itemPrice = 20;
+let totalCost = numberOFItems * itemPrice;
+let (sum) = customerName + " bought " + numberOfItems + " items for" + totalCost + "dollar" + ", each item is $" + itemPrice + ".");
+ 
+console.log(sum);
+
+
+
+Things to reflect on: 
+
+ - Why is it important to use meaningful variable names?
+
+Variables are important to have a better understanding of the purpose of codes for developers. 
+
+  - What are the common pitfalls to avoid when naming variables?
+
+  Using vague names, making names too long, mixing styles, using reserved words like function, giving misleading names.
+
+  - How do clear variable names benefit team collaboration?
+
+  it makes code easier to understand, and it reduces mistakes.
+  
+git add .
+git commit -m "Refactored variables and added userName and currentDate variables"
+git push origin main
